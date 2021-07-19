f8f655c1ae0d3cc19304502d190bf025c7bc34db
Cedric <cedric@Cedrics-MBP.lan>
2021-07-19 21:59:43 +0200

    rename back to server.js; shuffle files around; use fs for HTML file reading and serving

--- app/server.js
+++ app/server.js
@@ -1,5 +1,6 @@
 require('dotenv').config(); //Load ENV variables
 const http = require('http');
+const fs = require('fs');
 
 const hostname = '127.0.0.1';
 const port = process.env.PORT;
@@ -7,7 +8,18 @@ const port = process.env.PORT;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
-  res.end(`<h1>Hello, World!</h1>`);
+  switch(req.url){
+    case '/index': 
+      const indexFile = './app/index/index.html';
+      fs.stat(indexFile, (err, stats) => {
+        if(stats) {
+          fs.createReadStream(indexFile).pipe(res);
+        } else {
+          res.end('<h1>Hello, World!</h1>');
+        }
+      });
+      break;
+  }
 });
 
 server.listen(port, hostname, () => {
