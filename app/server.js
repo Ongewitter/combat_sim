9729089e3da3467d94ce7095ca59afdb69a0f91d
Cedric <cedric@Cedrics-MBP.lan>
2021-07-19 22:09:46 +0200

    Add more possible routes and basic routing

--- app/server.js
+++ app/server.js
@@ -8,18 +8,26 @@ const port = process.env.PORT;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
+  let htmlFile = '';
   switch(req.url){
+    case '/':
     case '/index': 
-      const indexFile = './app/index/index.html';
-      fs.stat(indexFile, (err, stats) => {
-        if(stats) {
-          fs.createReadStream(indexFile).pipe(res);
-        } else {
-          res.end('<h1>Hello, World!</h1>');
-        }
-      });
+      htmlFile = './app/index/index.html';
+      break;
+    case '/about': 
+      htmlFile = './app/about/about.html';
+      break;
+    case '/contact': 
+      htmlFile = './app/contact/contact.html';
       break;
   }
+  fs.stat(htmlFile, (err, stats) => {
+    if(stats) {
+      fs.createReadStream(htmlFile).pipe(res);
+    } else {
+      res.end('<h1>Hello, World!</h1>');
+    }
+  });
 });
 
 server.listen(port, hostname, () => {
