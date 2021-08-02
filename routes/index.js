b3f23f469a4af5f96e0f8b7f4487b8326e5e8848
Cedric <cedric@Cedrics-MBP.lan>
2021-08-02 17:17:42 +0200

    add writing to mongoDB

--- routes/index.js
+++ routes/index.js
@@ -13,13 +13,39 @@ router.get('/helloworld', function(req, res, next) {
 
 /* GET Userlist page. */
 router.get('/userlist', function(req, res) {
-  var db = req.db;
-  var collection = db.get('usercollection');
+  const db = req.db;
+  const collection = db.get('usercollection');
   collection.find({},{},function(e, docs){
       res.render('userlist', {
           "userlist" : docs,
           "title": "Hello World"
       });
+  });
+});
+
+/* GET New User page. */
+router.get('/newuser', function(_req, res) {
+  res.render('newuser', { title: 'Add New User' });
+});
+
+/* POST to Add User Service */
+router.post('/adduser', function(req, res) {
+  // Get the DB collection
+  const collection = req.db.get('usercollection');
+  // The form values.
+  const { userName, userEmail } = req.body;
+  const data = {
+    "username" : userName,
+    "email" : userEmail
+  }
+
+  // Submit to the DB
+  collection.insert(data, function (err, _doc) {
+      if (err) {
+          res.send("There was a problem adding the information to the database.");
+      } else {
+          res.redirect("userlist");
+      }
   });
 });
 
