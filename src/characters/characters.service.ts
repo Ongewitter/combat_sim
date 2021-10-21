02227368c91c5382908bf7344c5c3d6182719635
Cedric <cedric@Cedrics-MBP.lan>
2021-10-21 20:12:36 +0200

    skip validation for now

--- src/characters/characters.service.ts
+++ src/characters/characters.service.ts
@@ -1,7 +1,4 @@
 import { Injectable } from '@nestjs/common';
-import { check } from 'prettier';
-import { throwError } from 'rxjs';
-import { getSystemErrorMap } from 'util';
 import { CreateCharacterDto } from '../dtos/create_character';
 import { Character } from '../models/character';
 import { RULESETS } from '../data/rulesets';
@@ -13,16 +10,16 @@ export class CharacterService {
 
   create(characterDto: CreateCharacterDto) {
     // eslint-disable-next-line prefer-const
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     let { ruleset, ...characterOptions } = characterDto;
     const newCharacter = {
       id: this.characters.length,
       ...characterOptions,
     };
-
-console.log(RULESETS);
 console.log(ruleset);
-    if(!ruleset) { ruleset = 'DnD'; }
-    RULESETS[ruleset].validateCharacter(newCharacter);
+    // TODO Implement ruleset validation
+    // if(!ruleset) { ruleset = 'DnD'; }
+    // RULESETS[ruleset].validateCharacter(newCharacter);
 
     this.characters.push(newCharacter);
     return this.characters[this.characters.length - 1];
