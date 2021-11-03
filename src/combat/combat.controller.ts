2bde225b532f5d6f1fb06e1e10592691494ef0db
Cedric <cedric@Cedrics-MBP.lan>
2021-11-03 21:17:59 +0100

    remove obsolete comment

--- src/combat/combat.controller.ts
+++ src/combat/combat.controller.ts
@@ -5,13 +5,6 @@ import { CombatService } from './combat.service';
 export class CombatController {
   constructor(private combatService: CombatService) {}
 
-  // TODO: Need to return this:
-  // return (<TableRow key={result.id}>
-  //   <td>{result.status}</td>
-  //   <td>{result.name}</td>
-  //   <td>{result.hp}</td>
-  //   <td>{result.damage_dealt}</td>
-  //   <td>{result.damage_taken}</td>
   @Get()
   async index(){
     return this.combatService.combat();
