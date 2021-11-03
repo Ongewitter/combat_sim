42730712db9978c83a6770cb25ab9b0670afd118
Cedric <cedric@Cedrics-MBP.lan>
2021-11-03 19:38:53 +0100

    add MongoDB hooks to character service

--- src/characters/characters.service.ts
+++ src/characters/characters.service.ts
@@ -2,11 +2,26 @@ import { Injectable } from '@nestjs/common';
 import { CreateCharacterDto } from '../dtos/create_character';
 import { Character } from '../models/character';
 import { RULESETS } from '../data/rulesets';
+import { MongoClient } from 'mongodb';
+import { CONSTANTS } from '../constants';
 
 // This service is responsible for data storage and retrieval
 @Injectable()
 export class CharacterService {
   private characters: Array<Character> = [];
+  private charactersCollection = null;
+
+  constructor() {
+    MongoClient.connect(CONSTANTS.DB_URL, {}, (err, client) => {
+      if (err) {
+        return console.log(err);
+      }
+
+      // Storing a reference to the database so you can use it later
+      const db = client.db(CONSTANTS.DB_NAME);
+      this.charactersCollection = db.collection('characters');
+    });
+  }
 
   index(){
     return this.characters;
@@ -24,6 +39,10 @@ export class CharacterService {
     // if(!ruleset) { ruleset = 'DnD'; }
     // RULESETS[ruleset].validateCharacter(newCharacter);
     this.characters.push(newCharacter);
+    this.charactersCollection
+      .insertOne(newCharacter)
+      .then((result) => console.log(result))
+      .catch((error) => console.error(error));
     return this.characters[this.characters.length - 1];
   }
 
